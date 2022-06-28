eb1ebd0a7dd61456e78bdb8e14cd42a04e8bd476
Gustavo <gustavo.cesar.nunes@gmail.com>
2022-06-29 09:00:45 +1000

    commiting the project

---
+++ cliente.c
@@ -0,0 +1,220 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <string.h>
+#include <errno.h>
+#include <strings.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+
+#include "basic.h"
+#include "socket_helper.h"
+#define EXIT_COMMAND "exit\n"
+
+#define MAXLINE 4096
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
+
+int vidas = 6;
+int tamanho = 0;
+char letra;
+
+/* copiado do livro */
+ssize_t Readline (int fd, void *vptr, size_t maxlen) {
+    ssize_t n, rc;
+    char c, *ptr;
+
+    ptr = vptr;
+    for (n=1; n<maxlen; n++) {
+        again:
+        if ( (rc = read(fd, &c, 1)) == 1) {
+            *ptr++ = c;
+            if (c == '\n')
+                break; /* newline is stored, like fgets() */
+        } else if (rc == 0) {
+            if (n == 1)
+                return (0); /* EOF, no data read */
+            else
+                break; /* EOF, some data was read */
+        } else {
+            if (errno == EINTR)
+                goto again;
+            return (-1); /* error, errno set by read() */
+        }
+    }
+
+    *ptr = 0; /* null terminate like fgets() */
+    return(n);
+}
+
+void doit(int sockfd);
+
+int main(int argc, char **argv) {
+  int    port, sockfd, opcao, retval;
+  char * ip;
+  char   error[MAXLINE + 1];
+  struct sockaddr_in servaddr;
+  fd_set rset;
+  struct timeval tv;
+  char recvline[MAXLINE];
+
+  if (argc != 3) {
+    strcpy(error,"uso: ");
+    strcat(error,argv[0]);
+    strcat(error," <IPaddress, Port>");
+    perror(error);
+    exit(1);
+  }
+
+  // setup
+  ip = argv[1];
+  port = atoi(argv[2]);
+  sockfd = Socket(AF_INET, SOCK_STREAM, 0);
+  servaddr = ClientSockaddrIn(AF_INET, ip, port);
+  Connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+  // recebe o tamanho da palavra do servidor
+  Readline(sockfd, recvline, MAXLINE);
+  sscanf(recvline, "%d", &tamanho);
+
+  printf("\n\nWelcome to the Hangman game!\n \n \nPress 1 to start. \n");
+  scanf("%d", &opcao);
+
+  if (opcao == 1) {
+    printf("\nType a letter and press Enter to see if the word contains this letter. To guess the word, type it and press Enter.\n");
+    printf("\nThe game has begun!\n\n");
+    doit(sockfd);
+  }
+  else {
+    printf("Sorry, wrong option.\n");
+    write(sockfd, EXIT_COMMAND, strlen(EXIT_COMMAND));
+  }
+   exit(0);
+}
+
+void doit(int sockfd) {
+    fd_set fdset;
+    int maxfds, i, j, l, letrasRestantes = tamanho, lines = 0, ch = 0, position = 0;
+    char recvline[MAXLINE], rdline[MAXLINE], temp[MAXLINE], vetorPalavra[tamanho];
+    bool esperandoResposta = false, letraUsada[26]; // 70 entrada no vetor letraUsada para incluir extended ascii
+
+    FD_ZERO(&fdset);
+    // seta as letras iniciais como vazias
+    for (int i = 0; i < tamanho; i++)
+      vetorPalavra[i] = '_';
+    // setup do vetor de letras ja usadas
+    for (int i = 0; i < 26; i++)
+      letraUsada[i] = false;
+
+    // loop principal
+    while(vidas > 0) {
+      printf("\n============================================\n\n");
+      printf("The size of the word is: %d \n", tamanho);
+      printf("You have %d lifes.\n", vidas);
+
+      for (int i = 0; i < tamanho; i++) {
+        printf("%c ", vetorPalavra[i]);
+      }
+      if (letrasRestantes == 0) {
+        printf("\n\nCongratulations! You win!\n");
+        write(sockfd, EXIT_COMMAND, strlen(EXIT_COMMAND));
+        return;
+      }
+      printf("\n\nType a letter or try to guess the word:\n");
+
+      // loop de leitura, envio e recebimento
+      while(true) {
+        FD_SET(STDIN_FILENO, &fdset);
+        FD_SET(sockfd, &fdset);
+        maxfds = MAX(STDIN_FILENO, sockfd) + 1;
+        select(maxfds, &fdset, NULL, NULL, NULL);
+        // atividade no STDIN, só lê se não estiver esperando resposta do servidor
+        if (FD_ISSET(STDIN_FILENO, &fdset) && !esperandoResposta) {
+          Readline(STDIN_FILENO, rdline, MAXLINE);
+          // recebeu um caractere ascii e um \n
+          if (strlen(rdline) == 2) {
+              if (!isalpha(rdline[0])) { // não é uma letra
+                  printf("\nType a letter or a word without spaces, please.\n\n");
+                  continue; // volta a esperar input
+              }
+              rdline[0] = tolower(rdline[0]);
+              letra = tolower(rdline[0]);
+              l = letra-'a'; // converte pra posição na tabela ascii relativa ao caractere a
+              if (letraUsada[l]) {
+                printf("\nYou have already tried to use this letter! Please, try another one:\n");
+                continue; // volta a esperar input
+              }
+              else letraUsada[l] = true; // marca como usada
+          }
+          // caractere não-ascii com \n
+          if ( strlen(rdline) == 3 && !isalpha(rdline[0]) && !isalpha(rdline[1]) ) {
+            printf("\nInvalid character! Type a letter or a word without spaces, please.\n");
+            // tira vida e checa se acabou o jogo
+            vidas--;
+            if (vidas <= 0) {
+              printf("You lose!\n");
+              write(sockfd, EXIT_COMMAND, strlen(EXIT_COMMAND));
+              return;
+            }
+            else
+              printf("You have %d lifes.\n", vidas);
+              printf("Type a letter or a word:\n");
+            continue; // volta a esperar input
+          }
+          write(sockfd, rdline, strlen(rdline));
+          if (strcmp(rdline, EXIT_COMMAND) == 0)
+            return;
+          esperandoResposta = true;
+        }
+
+        // atividade no socket
+        if (FD_ISSET(sockfd, &fdset) && esperandoResposta) {
+          Readline(sockfd, recvline, MAXLINE);
+          i = 0;
+          // itera em cima do que foi recebido para decodificar a string
+          while (recvline[i] != '\n') {
+            if (recvline[i] == '#') { // indicador de derrota
+              vidas = -1;
+              printf("This is not the correct word. You lose!\n");
+              write(sockfd, EXIT_COMMAND, strlen(EXIT_COMMAND));
+            }
+            else if (recvline[i] == '!') { // indicador de vitoria
+              vidas = -1;
+              printf("Congratulations!! You win!\n");
+              write(sockfd, EXIT_COMMAND, strlen(EXIT_COMMAND));
+            }
+            else if (recvline[i] == ';') { // separador na string de resposta
+              i++;
+              continue;
+            }
+            else if (recvline[i] == '0') { // letra chutada não existe na palavra
+              vidas--;
+            }
+            else { // letra válida, recebeu uma lista de posições da letra enviada
+              j = 0;
+              memset(temp, 0, MAXLINE);
+              temp[j] = recvline[i];
+              while ((recvline[i+1] != ';') && (recvline[i+1] != '\n')) {  //lógica para posições com mais de um digito
+                i++;
+                j++;
+                temp[j] = recvline[i];
+              }
+              sscanf(temp, "%d", &position);
+              vetorPalavra[position-1] = letra;
+              letrasRestantes--;
+            }
+            i++;
+          }
+          esperandoResposta = false;
+          break;
+        } // fim do socket
+      } // fim do while true
+    } // fim do while vidas > 0
+    if (vidas != -1) {
+      printf("You lose!\n");
+      write(sockfd, EXIT_COMMAND, strlen(EXIT_COMMAND));
+    }
+}
---
+++ servidor.c
@@ -0,0 +1,149 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <time.h>
+#include <strings.h>
+#include <arpa/inet.h>
+#include <ctype.h>
+
+#include "basic.h"
+#include "socket_helper.h"
+
+#define LISTENQ 10
+#define MAXDATASIZE 4096
+#define EXIT_COMMAND "exit\n"
+
+void doit(int connfd, struct sockaddr_in clientaddr, char *word);
+char   *word = NULL;
+
+int main (int argc, char **argv) {
+   int    listenfd,
+          connfd,
+          port;
+   struct sockaddr_in servaddr;
+   char   error[MAXDATASIZE + 1];
+   char wordsize[100];
+   size_t len = MAXDATASIZE;
+   ssize_t read;
+
+   if (argc != 2) {
+      strcpy(error,"uso: ");
+      strcat(error,argv[0]);
+      strcat(error," <Port>");
+      perror(error);
+      exit(1);
+   }
+
+   FILE *fp = fopen("dicionario.txt","r");
+   if (fp == NULL)
+      exit(EXIT_FAILURE);
+
+   // setup
+   port = atoi(argv[1]);
+   listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+   servaddr = ServerSockaddrIn(AF_INET, INADDR_ANY, port);
+   Bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+   Listen(listenfd, LISTENQ);
+
+   for ( ; ; ) {
+      pid_t pid;
+
+      struct sockaddr_in clientaddr;
+      socklen_t clientaddr_len = sizeof(clientaddr);
+
+      connfd = Accept(listenfd, (struct sockaddr *) &clientaddr, &clientaddr_len);
+
+      word = NULL;
+      // pega uma palavra do dicionario
+      if ((read = getline(&word, &len, fp)) == -1) { // fim do arquivo ou falha
+        fp = fopen("dicionario.txt","r"); // loop pro começo do arquivo
+        if ((read = getline(&word, &len, fp)) == -1) // se falhar mesmo tentando abrir o arquivo de novo
+            exit(EXIT_FAILURE);
+      }
+
+      // indica tamanho da palavra pro cliente mostrar para o usuario
+      sprintf(wordsize, "%lu", strlen(word)-1);
+      strcat(wordsize, "\n");
+
+      write(connfd, &wordsize, strlen(wordsize));
+
+      if((pid = fork()) == 0) {
+         Close(listenfd);
+         fclose(fp);
+         // logica dos processos filhos
+         doit(connfd, clientaddr, word);
+         Close(connfd);
+         exit(0);
+      }
+      free(word);
+      Close(connfd);
+   }
+   fclose(fp);
+   return(0);
+}
+
+// Verifica se c está em word; se estiver, retorna uma string indicando as posições de todas ocorrencias
+void find_char(char * word, char c, char * returnString) {
+   int i, f=0, lenf=0;
+   char temp[MAXDATASIZE];
+   lenf = strlen(word);
+
+   sprintf(returnString, "");
+
+   for(i=0;i<lenf;i++) {
+      if(word[i] == c) {
+          sprintf(temp, "%d;", i+1);
+          strcat(returnString, temp);
+      }
+   }
+   if (strcmp(returnString, "") == 0)
+     strcat(returnString, "0");
+   strcat(returnString, "\n");
+   return;
+}
+
+void doit(int connfd, struct sockaddr_in clientaddr, char *word) {
+   char recvline[MAXDATASIZE + 1];
+   char sendline[MAXDATASIZE + 1];
+   int n;
+   int position;
+   char pos[2];
+   socklen_t remoteaddr_len = sizeof(clientaddr);
+
+   while (1) {
+      n = read(connfd, recvline, MAXDATASIZE);
+      recvline[n] = 0;
+      if (getpeername(connfd, (struct sockaddr *) &clientaddr, &remoteaddr_len) == -1) {
+         perror("getpeername() failed");
+         return;
+      }
+
+      // comando de finalização
+      if(strcmp(recvline, EXIT_COMMAND) == 0) {
+         printf("Client <%s-%d> disconnected.\n", inet_ntoa(clientaddr.sin_addr),(int) ntohs(clientaddr.sin_port));
+         break;
+      }
+
+      if (n == 2) { // recebeu um caractere
+        if(isalpha(recvline[0])) {    //verifica se o caracter recebido é uma letra
+          find_char(word, recvline[0], sendline); //procura na palavra a letra recebida do cliente e retorna as posições encontradas
+          write(connfd, sendline, strlen(sendline));
+        }
+        else { //se não for uma letra, diminui uma vida e avisa o cliente
+          sprintf(sendline, "*\n");
+          write(connfd, sendline, strlen(sendline));
+        }
+      }
+      else { // recebeu uma tentativa de palavra
+        if(strcmp(recvline, word) == 0)
+          sprintf(sendline, "!\n"); // sinaliza que ganhou o jogo
+        else
+          sprintf(sendline, "#\n"); // sinaliza que perdeu o jogo
+        write(connfd, sendline, strlen(sendline));
+      }
+   }
+}
